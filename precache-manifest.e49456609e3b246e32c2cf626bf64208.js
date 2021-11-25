b91f7d8b4d4fe6a48017befb5f38cfec79b09e14
javifm862 <javifm862@users.noreply.github.com>
2021-11-25 11:06:50 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.e49456609e3b246e32c2cf626bf64208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b347a4a90153d2bbe71b7093b4544638",
+    "url": "https://javifm862.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1855a4e7cd37179c8bf7",
+    "url": "https://javifm862.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "30b9a08e8f9e214c9b8c",
+    "url": "https://javifm862.github.io/task-management-frontend/static/js/2.b3722924.chunk.js"
+  },
+  {
+    "revision": "1855a4e7cd37179c8bf7",
+    "url": "https://javifm862.github.io/task-management-frontend/static/js/main.8269ccbe.chunk.js"
+  },
+  {
+    "revision": "5acc11ba8d4bc9138cfe",
+    "url": "https://javifm862.github.io/task-management-frontend/static/js/runtime~main.b980729d.js"
+  }
+]);
